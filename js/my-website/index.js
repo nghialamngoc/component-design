609c567658ea233223b9baa33c056d8a30068b77
Nghia.Lam <nghialamngocit@gmail.com>
2019-05-04 11:19:32 +0700

    some code

--- js/my-website/index.js
+++ js/my-website/index.js
@@ -1,6 +1,9 @@
 var menuBar = document.getElementById("menu-bar");
 var listSubject = document.getElementsByClassName("list-subject")[0];
+
 menuBar.addEventListener('click', menuBarClick);
+
+window.onscroll = function() {scrollFunction()};
 
 window.addEventListener('resize', function(event){
   if(window.innerWidth > 991)
@@ -19,4 +22,17 @@ function menuBarClick(){
     menuBar.className = "fas fa-bars";
   }
     
+}
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {    
+    document.getElementsByClassName("up_to_top")[0].style.display = "block";
+  } else {    
+    document.getElementsByClassName("up_to_top")[0].style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
